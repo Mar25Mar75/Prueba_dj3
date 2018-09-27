eb7548efbbd7ecdda3cb4442ffed0cb7d45cc427
Marcelo Fratoni <marcelo@lomasoft.com.ar>
2018-09-27 15:19:46 -0300

    Cambie Probando

---
+++ polls/urls.py
@@ -0,0 +1,16 @@
+'''
+Created on 26 mar. 2018
+
+@author: Marcelo
+'''
+from django.urls import path
+
+from . import views
+app_name = 'polls'
+urlpatterns = [
+    # ex: /polls/
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
